e115ce3e0d129b78de53702c363105f2504c12e2
Dylan <40326908+ipfsnut@users.noreply.github.com>
2024-04-20 15:44:30 -0500

    Update SyndicateFrameNFT.sol
    Added Degen payments, set wallet for Minting operations, added Withdraw function.

--- contracts/src/SyndicateFrameNFT.sol
+++ contracts/src/SyndicateFrameNFT.sol
@@ -6,12 +6,15 @@ pragma solidity ^0.8.20;
 
 import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721//ERC721.sol";
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// $DEGEN token contract address on Base network
+address constant DEGEN_TOKEN_ADDRESS = 0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed;
 
 contract SyndicateFrameNFT is ERC721, Ownable {
     uint256 public currentTokenId = 0;
     string public defaultURI;
 
-    mapping(address authorizedMinter => bool authorized) public authorizedMinters;
     mapping(uint256 tokenId => string tokenURI) public tokenURIs;
     mapping(uint256 tokenId => bool locked) public lockedTokenURIs;
 
@@ -22,12 +25,6 @@ contract SyndicateFrameNFT is ERC721, Ownable {
     event DefaultTokenURISet(string tokenURI);
     event TokenURISet(uint256 indexed tokenId, string tokenURI);
     event TokenURILocked(uint256 indexed tokenId);
-    event AuthorizedMinterSet(address indexed minter, bool authorized);
-
-    modifier onlyAuthorizedMinter() {
-        require(authorizedMinters[msg.sender], "FrameNFTs: Mint must be triggered by API");
-        _;
-    }
 
     modifier onlyUnlockedTokenURI(uint256 tokenId) {
         require(!lockedTokenURIs[tokenId], "FrameNFTs: Token URI is locked");
@@ -39,35 +36,26 @@ contract SyndicateFrameNFT is ERC721, Ownable {
         _;
     }
 
-    // The deployer is set as the initial owner by default. Make sure to
-    // transfer this to a Safe or other multisig for long-term use!
-    // You can call `transferOwnership` to do this.
+    uint256 public collectedDEGEN;
+
     constructor() ERC721("SyndicateFrameNFT", "SYNFRAME") Ownable(msg.sender) {
-        // Update this with your own NFT collection's metadata
         defaultURI = "ipfs://QmSFqezaUhBKr32Z2vgFrbDPGYdbcj8zQcQvsDqbU6b6UH";
         maxMintPerAddress = 1;
-
-        // The deployer is set as an authorized minter, allowing them to set up
-        // owner mints manually via the contract as needed
-        authorizedMinters[msg.sender] = true;
-        emit AuthorizedMinterSet(msg.sender, true);
-
-        // Authorize Syndicate's API-based wallet pool as a minter on Base
-        // Mainnet
-        authorizeBaseMainnetSyndicateAPI();
     }
 
-    // This function is currently the only supported function in the
-    // frame.syndicate.io API
-    function mint(address to) public onlyAuthorizedMinter onlyBelowMaxMint(to) {
+    function mint(address to) public onlyBelowMaxMint(to) {
+        // Charge 100 $DEGEN tokens for the mint
+        require(IERC20(DEGEN_TOKEN_ADDRESS).transferFrom(msg.sender, address(this), 100 * 10**18), "FrameNFTs: Failed to transfer $DEGEN");
+
         ++currentTokenId;
         ++mintCount[to];
         _mint(to, currentTokenId);
+
+        // Update the collected $DEGEN amount
+        collectedDEGEN += 100 * 10**18;
     }
 
-    // This function is not yet supported in the frame.syndicate.io API
-    // We will update this example repository when it is supported!
-    function mint(address to, string memory _tokenURI) public onlyAuthorizedMinter onlyBelowMaxMint(to) {
+    function mint(address to, string memory _tokenURI) public onlyBelowMaxMint(to) {
         ++currentTokenId;
         ++mintCount[to];
         tokenURIs[currentTokenId] = _tokenURI;
@@ -76,11 +64,8 @@ contract SyndicateFrameNFT is ERC721, Ownable {
         emit TokenURISet(currentTokenId, _tokenURI);
     }
 
-    // This function is not yet supported in the frame.syndicate.io API
-    // We will update this example repository when it is supported!
     function setTokenURI(uint256 tokenId, string memory _tokenURI)
         public
-        onlyAuthorizedMinter
         onlyUnlockedTokenURI(tokenId)
     {
         tokenURIs[tokenId] = _tokenURI;
@@ -88,26 +73,17 @@ contract SyndicateFrameNFT is ERC721, Ownable {
         emit TokenURISet(tokenId, _tokenURI);
     }
 
-    // Since this action is irreversible, we require the owner to call it
     function lockTokenURI(uint256 tokenId) public onlyOwner {
         lockedTokenURIs[tokenId] = true;
 
         emit TokenURILocked(tokenId);
     }
 
-    // Set the token URI for all tokens that don't have a custom tokenURI set.
-    // Must be called by the owner given its global impact on the collection
     function setDefaultTokenURI(string memory _tokenURI) public onlyOwner {
         defaultURI = _tokenURI;
         emit DefaultTokenURISet(_tokenURI);
     }
 
-    // If you'd like to use an ID-based tokenURI (e.g. /1, /2, etc), you can
-    // override this function The majority of Frames use cases are relying on
-    // dynamically generated data, so our assumption is that a separate URI for
-    // each token + a default URI for unset tokens is the most useful
-    // If you'd prefer an ID-based structure, you can override this function to
-    // enable that instead
     function tokenURI(uint256 tokenId) public view virtual override(ERC721) returns (string memory) {
         if (bytes(tokenURIs[tokenId]).length > 0) {
             return tokenURIs[tokenId];
@@ -116,39 +92,16 @@ contract SyndicateFrameNFT is ERC721, Ownable {
         }
     }
 
-    // Only the owner can set the max mint per address
     function setMaxMintPerAddress(uint256 _maxMintPerAddress) public onlyOwner {
         maxMintPerAddress = _maxMintPerAddress;
     }
 
-    // Only the owner can set authorized minters. True = authorized, false =
-    // unauthorized
-    function setAuthorizedMinter(address minter, bool authorized) public onlyOwner {
-        authorizedMinters[minter] = authorized;
-
-        emit AuthorizedMinterSet(minter, authorized);
+    function withdrawDEGEN(uint256 amount) public onlyOwner {
+        require(amount <= collectedDEGEN, "FrameNFTs: Insufficient $DEGEN balance");
+        require(IERC20(DEGEN_TOKEN_ADDRESS).transfer(msg.sender, amount), "FrameNFTs: Failed to transfer $DEGEN");
+        collectedDEGEN -= amount;
     }
 
-    // These addresses are for Base Mainnet only. Contact @will on Farcaster
-    // or @WillPapper on Telegram if you need other networks
-    // If you've set up your own Syndicate account, you can change this function
-    // to your own wallet addresses
-    function authorizeBaseMainnetSyndicateAPI() internal {
-        authorizedMinters[0x3D0263e0101DE2E9070737Df30236867485A5208] = true;
-        authorizedMinters[0x98407Cb54D8dc219d8BF04C9018B512dDbB96caB] = true;
-        authorizedMinters[0xF43A72c1a41b7361728C83699f69b5280161F0A5] = true;
-        authorizedMinters[0x94702712BA81C0D065665B8b0312D87B190EbA37] = true;
-        authorizedMinters[0x10FD71C6a3eF8F75d65ab9F3d77c364C321Faeb5] = true;
-
-        emit AuthorizedMinterSet(0x3D0263e0101DE2E9070737Df30236867485A5208, true);
-        emit AuthorizedMinterSet(0x98407Cb54D8dc219d8BF04C9018B512dDbB96caB, true);
-        emit AuthorizedMinterSet(0xF43A72c1a41b7361728C83699f69b5280161F0A5, true);
-        emit AuthorizedMinterSet(0x94702712BA81C0D065665B8b0312D87B190EbA37, true);
-        emit AuthorizedMinterSet(0x10FD71C6a3eF8F75d65ab9F3d77c364C321Faeb5, true);
-    }
-
-    // This function ensures that ETH sent directly to the contract by mistake
-    // is rejected
     fallback() external payable {
         revert("FrameNFTs: Does not accept ETH");
     }
