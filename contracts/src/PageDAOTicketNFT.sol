134ca578181c87ffcfb19c7fbc05a43bca110d37
Dylan <40326908+ipfsnut@users.noreply.github.com>
2024-04-21 03:01:58 -0500

    Update PageDAOTicketNFT.sol
    few more hours of iterations... thinking this one *should* work, but can't test the degen functionality in remix (or would have!)

--- contracts/src/PageDAOTicketNFT.sol
+++ contracts/src/PageDAOTicketNFT.sol
@@ -1,73 +1,76 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "../.deps/npm/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../.deps/npm/@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "../.deps/npm/@openzeppelin/contracts/access/Ownable.sol";
-import "../.deps/npm/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract PageDAOTicketNFT is ERC721, ERC721Burnable, Ownable {
     uint256 public currentTokenId = 0;
     string public defaultURI;
-
     mapping(uint256 => string) public tokenURIs;
     mapping(uint256 => bool) public lockedTokenURIs;
-
-    uint256 public collectedDEGEN;
-
-    // $DEGEN token contract address
-    address constant DEGEN_TOKEN_ADDRESS = 0x888F05D02ea7B42f32f103C089c1750170830642;
-
-    // Mapping to track addresses that have minted a free NFT
     mapping(address => bool) public hasMintedFree;
 
-    constructor(string memory _defaultURI, address initialOwner) ERC721("PageDAO Ticket NFT", "TICK1") Ownable(initialOwner) {
+    uint256 public collectedDEGEN = 0;
+    IERC20 public immutable degenToken;
+    address public immutable degenRecipient;
+
+    constructor(string memory _defaultURI, address initialOwner, address _degenRecipient) ERC721("PageDAOTicketNFT", "PDAO") Ownable(initialOwner) {
         defaultURI = _defaultURI;
+        degenToken = IERC20(0x888F05D02ea7B42f32f103C089c1750170830642);
+        degenRecipient = _degenRecipient;
     }
 
-    function mintFree(address to) public {
+    function mintFree(address to) external {
         require(!hasMintedFree[msg.sender], "You have already minted a free NFT");
-        _mint(to, currentTokenId);
+        _safeMint(to, currentTokenId);
         currentTokenId++;
         hasMintedFree[msg.sender] = true;
     }
 
-    function mintWithDEGEN(address to) public {
-        // Charge 100 $DEGEN tokens for the mint
-        require(IERC20(DEGEN_TOKEN_ADDRESS).transferFrom(msg.sender, address(this), 100 * 10**18), "Failed to transfer $DEGEN");
-
-        _mint(to, currentTokenId);
+    function mintWithDEGEN(address to, uint256 degenAmount) external {
+        require(degenAmount >= 100 * 10 ** 18, "Minimum DEGEN amount is 100");
+        require(degenToken.transferFrom(msg.sender, address(this), degenAmount), "DEGEN transfer failed");
+        _safeMint(to, currentTokenId);
         currentTokenId++;
-
-        // Update the collected $DEGEN amount
-        collectedDEGEN += 100 * 10**18;
+        collectedDEGEN += degenAmount;
     }
 
-    function setTokenURI(uint256 tokenId, string memory _tokenURI) public onlyOwner {
-        require(!lockedTokenURIs[tokenId], "Token URI is locked");
-        tokenURIs[tokenId] = _tokenURI;
+    function approveDEGEN(uint256 degenAmount) external {
+        require(degenToken.approve(address(this), degenAmount), "DEGEN approval failed");
     }
 
-    function lockTokenURI(uint256 tokenId) public onlyOwner {
-        lockedTokenURIs[tokenId] = true;
+    function mint(address to) external onlyOwner {
+        _safeMint(to, currentTokenId);
+        currentTokenId++;
     }
 
-    function setDefaultTokenURI(string memory _tokenURI) public onlyOwner {
+    function setDefaultTokenURI(string memory _tokenURI) external onlyOwner {
         defaultURI = _tokenURI;
     }
 
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) external onlyOwner {
+        tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function lockTokenURI(uint256 tokenId) external onlyOwner {
+        lockedTokenURIs[tokenId] = true;
+    }
+
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        if (bytes(tokenURIs[tokenId]).length > 0) {
+        if (lockedTokenURIs[tokenId]) {
             return tokenURIs[tokenId];
         } else {
             return defaultURI;
         }
     }
 
-    function withdrawDEGEN() public onlyOwner {
-        uint256 amount = collectedDEGEN;
-        require(amount > 0, "No $DEGEN to withdraw");
-        require(IERC20(DEGEN_TOKEN_ADDRESS).transfer(msg.sender, amount), "Failed to transfer $DEGEN");
+    function withdrawDEGEN() external onlyOwner {
+        uint256 balance = collectedDEGEN;
+        require(balance > 0, "No DEGEN to withdraw");
         collectedDEGEN = 0;
+        degenToken.transfer(degenRecipient, balance);
     }
 }
